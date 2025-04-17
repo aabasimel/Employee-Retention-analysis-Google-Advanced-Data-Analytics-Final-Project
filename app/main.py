d6e72da56a8fec2653364622c4da0f42505c208a
Ahmed <abasimelahmed.com>
2025-04-17 12:44:40 +0200

    Updated style.css file to reflect changes

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-local_css("style.css")
+local_css("app/style.css")
 
 def read_pickle(path, saved_model_name):
     with open(path + saved_model_name, 'rb') as to_read:
